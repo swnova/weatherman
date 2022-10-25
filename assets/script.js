3023af684c234d99c173b861403dc48b37cedbc9
swnova <sharrodwillanova@gmail.com>
2022-10-25 02:48:27 -0700

    added element for date

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var formSub = document.getElementById("search-city")
 // console.log(formSub)
 var apiKey = "b2679b430f27a003489d800bbcbe3e02"
 function getCoordinates(){
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+formSub.value+"&appid=b2679b430f27a003489d800bbcbe3e02")
+    fetch("https://api.openweathermap.org/geo/1.0/direct?q="+formSub.value+"&appid=b2679b430f27a003489d800bbcbe3e02")
     .then(function(response){
     //   console.log(response)
         return response.json()
@@ -16,15 +16,16 @@ getForcast(data[0].lat, data[0].lon)
 }
 
 function getForcast(lat,lon){
-    fetch("http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=b2679b430f27a003489d800bbcbe3e02&units=imperial")
+    fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=b2679b430f27a003489d800bbcbe3e02&units=imperial")
     .then(function(response){
         return response.json()
     })
     .then(function(data){
-        console.log(data)
-        console.log(data.list[0].main.temp)
-        console.log(data.list[0].main.humidity)
-        console.log(data.list[0].wind.speed)
+        let dayIs = document.getElementById('currentDate')
+        dayIs.textContent=data.list[0].dt_txt;
+        let cityName = document.getElementById('currentCity')
+        cityName.textContent=data.city.name;
+        console.log(cityName)
         let currenTemp = document.getElementById('currenTemp')
         currenTemp.textContent=data.list[0].main.temp;
         let windSpeed = document.getElementById('windSpeed')
@@ -33,6 +34,10 @@ function getForcast(lat,lon){
         humidity.textContent=data.list[0].main.humidity;
         console.log(data.list[6].main.temp)
 // Day One of Five Weather Information
+        // let dateOne = document.createElement('p');
+        // dateOne.textContent="today's date: "+data.list[6].dt_txt;
+        // dayOne.appendChild(dateOne)
+
         let tempOne = document.createElement('p');
         tempOne.textContent="temp: "+data.list[6].main.temp;
         var dayOne = document.getElementById('day1');
@@ -46,6 +51,10 @@ function getForcast(lat,lon){
         windOne.textContent="windspeed "+data.list[6].wind.speed;
         dayOne.appendChild(windOne);
 // Day Two of Five Information
+        // let dateTwo = document.createElement('p');
+        // dateTwo.textContent="date: "+data.list[14].dt_txt;
+        // dayTwo.appendChild(dateTwo);
+
         let tempTwo = document.createElement('p');
         tempTwo.textContent="temp: "+data.list[14].main.temp;
         var dayTwo = document.getElementById('day2');
@@ -58,7 +67,11 @@ function getForcast(lat,lon){
         let windTwo = document.createElement('p');
         windTwo.textContent="windspeed "+data.list[14].wind.speed;
         dayTwo.appendChild(windTwo);
-// Day Three of Five Information        
+// Day Three of Five Information       
+        // let dateThree = document.createElement('p');
+        // dateThree.textContent="date: "+data.list[22].dt_txt;
+        // dayThree.appendChild(dateThree);
+
         let tempThree = document.createElement('p');
         tempThree.textContent="temp: "+data.list[22].main.temp;
         var dayThree = document.getElementById('day3');
@@ -72,6 +85,10 @@ function getForcast(lat,lon){
         windThree.textContent="windspeed "+data.list[22].wind.speed;
         dayThree.appendChild(windThree);
 // Day Four of Five Information
+        // let dateFour = document.createElement('p');
+        // dateFour.textContent="date: "+data.list[30].dt_txt;
+        // dayFour.appendChild(dateFour);
+
         let tempFour = document.createElement('p');
         tempFour.textContent="temp: "+data.list[30].main.temp;
         var dayFour = document.getElementById('day4');
@@ -85,6 +102,10 @@ function getForcast(lat,lon){
         windFour.textContent="windspeed "+data.list[30].wind.speed;
         dayFour.appendChild(windFour);
 // Day Five of Five Information
+        // let dateFive = document.createElement('p');
+        // dateFive.textContent="date: "+data.list[38].dt_txt;
+        // dayFive.appendChild(dateFive);
+
         let tempFive = document.createElement('p');
         tempFive.textContent="temp: "+data.list[38].main.temp;
         var dayFive = document.getElementById('day5');
@@ -97,6 +118,7 @@ function getForcast(lat,lon){
         let windFive = document.createElement('p');
         windFive.textContent="windspeed "+data.list[38].wind.speed;
         dayFive.appendChild(windFive);
+        console.log(data)
     })
     
 }
@@ -105,8 +127,3 @@ searchBtn.addEventListener("click",function(){
     // console.log(formSub.value)
     getCoordinates()
 })
-
-
-//http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-// 5 day weather data
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
